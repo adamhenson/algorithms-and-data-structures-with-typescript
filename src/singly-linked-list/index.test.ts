f71ea10c85cf2e5be11e7cedd09d838df36d433d
adamhenson <adamhenson1979@gmail.com>
2022-07-12 00:32:46 -0400

    feat: singly linked list

--- src/singly-linked-list/index.test.ts
+++ src/singly-linked-list/index.test.ts
@@ -1,7 +1,57 @@
-import singlyLinkedList from '.';
+import SinglyLinkedList, { LinkedListNode } from '.';
 
-describe('singlyLinkedList', () => {
-  it('should return its name', () => {
-    expect(singlyLinkedList()).toEqual('singlyLinkedList');
+describe('SinglyLinkedList', () => {
+  let mockNode: LinkedListNode;
+
+  beforeEach(() => {
+    mockNode = {
+      data: 'a',
+      next: {
+        data: 'b',
+        next: null,
+      },
+    };
+  });
+
+  it('should instantiate with nodes', () => {
+    const list = new SinglyLinkedList(mockNode);
+    expect(list.head.data).toEqual('a');
+    expect(list.head.next.data).toEqual('b');
+  });
+
+  describe('SinglyLinkedList.find', () => {
+    it('should find a node by matching predicate', () => {
+      const list = new SinglyLinkedList(mockNode);
+      const bNode = list.find((node) => node.data === 'b');
+      expect(bNode.data).toEqual('b');
+    });
+  });
+
+  describe('SinglyLinkedList.insert', () => {
+    it('should insert a node', () => {
+      const list = new SinglyLinkedList(mockNode);
+      list.insert({
+        data: 'c',
+        next: null,
+      });
+      expect(list.head.next.next.data).toEqual('c');
+    });
+  });
+
+  describe('SinglyLinkedList.remove', () => {
+    it('should remove a node by matching predicate', () => {
+      const list = new SinglyLinkedList({
+        ...mockNode,
+        next: {
+          ...mockNode.next,
+          next: {
+            data: 'c',
+            next: null,
+          },
+        },
+      });
+      list.remove((node) => node.data === 'b');
+      expect(list.head.next.data).toEqual('c');
+    });
   });
 });
--- src/singly-linked-list/index.ts
+++ src/singly-linked-list/index.ts
@@ -1,5 +1,36 @@
-const singlyLinkedList = (): string => {
-  return 'singlyLinkedList';
-};
+export interface LinkedListNode {
+  data: any;
+  next: LinkedListNode | null;
+}
 
-export default singlyLinkedList;
+export default class SinglyLinkedList {
+  constructor(public head: LinkedListNode = null) {}
+
+  find(
+    predicate: (node: LinkedListNode, nextNode?: LinkedListNode) => boolean,
+  ): LinkedListNode | null {
+    let node = this.head;
+    while (node !== null) {
+      if (predicate(node, node.next)) {
+        return node;
+      }
+      node = node.next;
+    }
+    return null;
+  }
+
+  insert(node: LinkedListNode) {
+    const lastNode = this.find((node) => node.next === null);
+    lastNode.next = node;
+  }
+
+  remove(predicate: (node: LinkedListNode) => boolean): LinkedListNode | null {
+    const nodeBefore = this.find((_node, nextNode) => predicate(nextNode));
+    if (!nodeBefore) {
+      return null;
+    }
+    const { next: removedNode } = nodeBefore;
+    nodeBefore.next = removedNode.next;
+    return removedNode;
+  }
+}
