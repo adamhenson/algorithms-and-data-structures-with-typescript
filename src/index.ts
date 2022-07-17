bad2fcaae270620d4be983fd9e8cb7a2850e03fa
adamhenson <adamhenson1979@gmail.com>
2022-07-17 14:32:45 -0400

    fix: index imports

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,2 @@
-export { default as singlyLinkedList } from './data-structures/linear-data-structures/singly-linked-list';
+export { default as SinglyLinkedList } from './data-structures/linear-data-structures/singly-linked-list';
+export { default as Stack } from './data-structures/linear-data-structures/stack';
